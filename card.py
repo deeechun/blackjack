1577ee8aa6e439aaf17e0d70bcd9d42e1a0596fc
David <dechun92@gmail.com>
2016-11-01 16:39:56 -0700

    Add in card file containing Card class

---
+++ card.py
@@ -0,0 +1,13 @@
+class Card(object):
+	""" This class represents a standard card in a deck
+
+	Each card consists of two attributes: value and suit
+
+	There are 4 unique suits and 13 values """
+
+	def __init__(self, value, suit):
+		self.value = value
+		self.suit = suit
+
+
+

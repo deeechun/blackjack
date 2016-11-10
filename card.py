fd9a42f8c4e225836bf529c1651e4fb7c4e870e6
David <dechun92@gmail.com>
2016-11-10 14:33:55 -0800

    Add new method to get value of card

--- card.py
+++ card.py
@@ -5,9 +5,23 @@ class Card(object):
 
 	There are 4 unique suits and 13 values """
 
-	def __init__(self, value, suit):
-		self.value = value
+	def __init__(self, raw_card, value = None, suit = None):
+		self.raw_card = raw_card
+		self.value = self.get_value()
 		self.suit = suit
 
+	def get_value(self):
+		""" Given a card string in format "[value][suit]", it will return the value of the card
 
+		Ex: "10S" represents 10 of spades and will return the value 10 """
+		face_cards = ["J","Q","K"]
+		ace = "A"
 
+		value = self.raw_card[:len(self.raw_card) - 1]
+		if value in face_cards:
+			value = 10
+		if value == "A":
+			value = 11
+		else:
+			value = int(value)
+		return value

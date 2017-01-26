5de6477af2f374f908cedff4f2fdb5115484bff5
David <dechun92@gmail.com>
2017-01-25 20:14:52 -0800

    Added in docstrings and separated functions for better visibility

--- card.py
+++ card.py
@@ -7,34 +7,31 @@ class Card(object):
 
 	def __init__(self, value, suit):
 
-		"""Attrs:
-
-		suit: represents the suit of a card, represented by the first letter of the suit
-		Ex: 'S' represents spades
-
-		raw_card: representative form of the Card object
-		Ex: AS represents the Ace of Spades
-
-		value: numerical value of the card
-		Ex: 10S will return 10
+		"""
+		:param value string that represents card value 
+		:param suit string that represents the suit of a card
+		:param raw_card string that combines value and suit (ex. 'Jack of Spades')
 		"""
 	
 		self.value = value
 		self.suit = suit
-		self.raw_card = value + suit
+		self.raw_card = value + " of " + suit
+
+	def get_raw_card(self):
+		return self.raw_card
 
 	def get_numerical_value_of_card(self):
 		"""Given a card string in format "[value][suit]", it will return the value of the card
+		Ex: "10S" represents 10 of spades and will return the value 10 
 
-		Ex: "10S" represents 10 of spades and will return the value 10 """
-		face_cards = ["J","Q","K"]
-		ace = "A"
-
+		:returns value numerical blackjack value of card
+		"""
+		face_cards = ["Jack","Queen","King"]
 		value = self.value
 
 		if value in face_cards:
 			value = 10
-		if value == "A":
+		if value == "Ace":
 			value = 11
 		else:
 			value = int(value)
